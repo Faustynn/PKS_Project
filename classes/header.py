fa3a8fc5550862f75390871e9ff2196f70979bc5
Nazar Faustyn <153837991+Faustynn@users.noreply.github.com>
2024-11-10 02:14:36 +0100

    Finall hs,kp

--- classes/header.py
+++ classes/header.py
@@ -1,19 +1,19 @@
 import struct
 import zlib
 
-def create_header(config, seq_num, ack_num, service_type, data_type, timer):
+ # Format
+def create_header(config, seq_num, ack_num, flags, window,checksum):
     checksum = 0
-    header_length = config.HEADER_SIZE
-    combined_service_data = (service_type << 4) | (data_type & 0x0F)
+    offset = config.HEADER_SIZE
 
     header = struct.pack(
         config.HEADER_FORMAT,
         seq_num,
         ack_num,
-        header_length,
-        combined_service_data,
+        offset,
+        flags,
+        window,
         checksum,
-        timer
     )
     checksum = calculate_checksum(header)
 
@@ -21,13 +21,14 @@ def create_header(config, seq_num, ack_num, service_type, data_type, timer):
         config.HEADER_FORMAT,
         seq_num,
         ack_num,
-        header_length,
-        combined_service_data,
+        offset,
+        flags,
+        window,
         checksum,
-        timer
     )
-
     return header
 
+
+# Checksum calculation throw crc32
 def calculate_checksum(data):
     return zlib.crc32(data) & 0xFFFF
--- classes/keep_alive.py
+++ classes/keep_alive.py
@@ -1,12 +1,17 @@
 import threading
 from classes.header import create_header
 
-def send_keep_alive(config, s, dest_ip, dest_port, seq_num, ack_num):
-    custom_header = create_header(config, seq_num, ack_num, config.TYPE_OF_SERVICE_KEEP_ALIVE,config.DATA_TYPE_KEEP_ALIVE, 0)
-    s.sendto(custom_header, (dest_ip, dest_port))
-    if config.DEBUG:
-        print(f"DEBUG: Sent KEEP ALIVE to {dest_ip}:{dest_port}")
+def send_keep_alive(config, socket, dest_ip, dest_port, seq_num, ack_num):
+    custom_header = create_header(config, seq_num, ack_num, config.FLAGS_KEEP_ALIVE,0, 0)
 
-    keep_alive_timer = threading.Timer(config.KEEP_ALIVE_INTERVAL, send_keep_alive, [config, s, dest_ip, dest_port, seq_num, ack_num])
+    try:
+        socket.sendto(custom_header, (dest_ip, dest_port))
+        if config.DEBUG:
+            print(f"DEBUG: Sent KEEP ALIVE to {dest_ip}:{dest_port}")
+    except Exception as e:
+        print(f"Error: {e}")
+        return
+
+    keep_alive_timer = threading.Timer(config.KEEP_ALIVE_INTERVAL, send_keep_alive, [config, socket, dest_ip, dest_port, seq_num, ack_num])
     keep_alive_timer.start()
     return keep_alive_timer
